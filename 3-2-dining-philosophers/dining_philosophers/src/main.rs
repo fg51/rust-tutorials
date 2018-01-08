784f38e6b98be537cc87dedbcbfe81ea821f14ad
kflange <zknsftk1msrk@gmail.com>
2018-01-08 15:00:48 +0900

    create philosophers

---
+++ 3-2-dining-philosophers/dining_philosophers/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michel Foucault"),
+    ];
+
+    for p in &philosophers {
+        p.eat();
+    }
+}
+
+
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
+}
