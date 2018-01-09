4bd3f19c5c15341be9419aff9b239417be46034c
kflange <zknsftk1msrk@gmail.com>
2018-01-09 12:38:26 +0900

    add simple test

--- testing/adder/src/lib.rs
+++ testing/adder/src/lib.rs
@@ -1,7 +1,23 @@
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_works_panic() {
+        assert!(false);
+    }
+
+    #[test]
+    fn test_add_two() {
+        assert_eq!(4, super::add_two(2));
     }
 }
